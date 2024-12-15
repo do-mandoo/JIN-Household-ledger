48914a01b47d1c8a66e32c2a5e53ecdb12af7c77
do-mandoo <67843356+do-mandoo@users.noreply.github.com>
2024-12-15 21:58:52 +0900

    idb적용 전 간단예제로 연습 및 익히기

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { createTheme, ThemeProvider } from '@mui/material';
 import './App.css';
 import MainPage from './pages/MainPage';
+import IDBPractice from './practice/idbPractice/IDBPractice';
 
 const theme = createTheme({
   typography: {
@@ -11,7 +12,8 @@ const theme = createTheme({
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <MainPage />
+      {/* <MainPage /> */}
+      <IDBPractice />
     </ThemeProvider>
   );
 }
